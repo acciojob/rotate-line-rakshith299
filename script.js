00dd3aa97e79cc486669ca2f624fc063b5ff2a3a
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-17 18:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function rotate(){
+
+    let timing = document.getElementById("line");
+    let degree = 0;
+
+    function rotateAtInterval(){
+        degree = degree + 5;
+        timing.style.transform = `rotate(${degree}deg)`;
+        timing.style.transitionTimingFunction = 'linear';
+        timing.style.transitionDuration = '0.4s'; 
+        
+    }
+
+    setInterval(rotateAtInterval,100)
+
+}
+
+rotate();
